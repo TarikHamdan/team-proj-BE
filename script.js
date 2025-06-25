c3858846de330027ec1109294943bfd517a97d3f
Tarik <tarikhamdan42@gmail.com>
2025-06-25 06:57:45 +0300

    initialize task management DOM elements

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 let currentFilter = "all";
 let editIndex = null;
+
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const warning = document.getElementById("warning");
+const filterButtons = document.querySelectorAll(".filter");
